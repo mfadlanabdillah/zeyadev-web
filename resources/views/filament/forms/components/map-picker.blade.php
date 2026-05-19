d3dfec738d203f376f0c0b90b132e5ea2ddecab0
Mochamad Fadlan Abdillah <mochamadfadlanabdillah@gmail.com>
2026-05-19 10:40:21 +0700

    fix: use this.$refs.map instead of passing container param

--- resources/views/filament/forms/components/map-picker.blade.php
+++ resources/views/filament/forms/components/map-picker.blade.php
@@ -1,6 +1,6 @@
 <div
     x-data="mapPicker()"
-    x-init="init($refs.map)"
+    x-init="init()"
     wire:ignore
     class="rounded-lg overflow-hidden border border-gray-200 dark:border-gray-700"
     style="position: relative;"
@@ -14,9 +14,12 @@ function mapPicker() {
         map: null,
         marker: null,
         initialized: false,
-        init(container) {
+        init() {
             if (this.initialized || typeof L === 'undefined') return
             this.initialized = true
+
+            const container = this.$refs.map
+            if (!container) return
 
             const latInput = document.querySelector('[data-field-name="latitude"] input')
             const lngInput = document.querySelector('[data-field-name="longitude"] input')
