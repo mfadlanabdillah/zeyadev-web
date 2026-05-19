4887128039da98fcdc574b46c5b4fc37ef5ad78f
Mochamad Fadlan Abdillah <mochamadfadlanabdillah@gmail.com>
2026-05-19 10:58:25 +0700

    Revert "fix: simplify map picker view to use plain JS"
    This reverts commit e955f21f08bfdec47c390270c314878e0bc2e654.

--- resources/views/filament/forms/components/map-picker.blade.php
+++ resources/views/filament/forms/components/map-picker.blade.php
@@ -1,71 +1,72 @@
-<div
-    x-data="mapPicker()"
-    x-init="init($refs.map)"
-    wire:ignore
-    class="rounded-lg overflow-hidden border border-gray-200 dark:border-gray-700"
-    style="position: relative;"
+<x-dynamic-component
+    :component="$getFieldWrapperView()"
+    :field="$field"
 >
-    <div x-ref="map" style="height: 400px;"></div>
-</div>
+    <style>
+        .leaflet-map-container { position: relative; overflow: hidden; }
+        .leaflet-map-container .leaflet-container { position: relative !important; }
+    </style>
 
-<script>
-function mapPicker() {
-    return {
-        map: null,
-        marker: null,
-        initialized: false,
-        init(container) {
-            if (this.initialized || typeof L === 'undefined') return
-            this.initialized = true
+    <div
+        x-data="{
+            state: @entangle($getStatePath()),
+            lat: @entangle($getStatePath('latitude')),
+            lng: @entangle($getStatePath('longitude')),
+            map: null,
+            marker: null,
+            initialized: false,
+            init() {
+                if (this.initialized) return
+                this.initialized = true
 
-            const latInput = document.querySelector('[data-field-name="latitude"] input')
-            const lngInput = document.querySelector('[data-field-name="longitude"] input')
+                const defaultLat = this.lat || -6.200000
+                const defaultLng = this.lng || 106.845000
 
-            const defaultLat = latInput ? parseFloat(latInput.value) || -6.200000 : -6.200000
-            const defaultLng = lngInput ? parseFloat(lngInput.value) || 106.845000 : 106.845000
+                delete L.Icon.Default.prototype._getIconUrl
+                L.Icon.Default.mergeOptions({
+                    iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+                    iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+                    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png'
+                })
 
-            delete L.Icon.Default.prototype._getIconUrl
-            L.Icon.Default.mergeOptions({
-                iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
-                iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-                shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png'
-            })
+                this.map = L.map(this.$refs.map, { zoomControl: false }).setView([defaultLat, defaultLng], 13)
+                L.control.zoom({ position: 'topright' }).addTo(this.map)
 
-            this.map = L.map(container, { zoomControl: false }).setView([defaultLat, defaultLng], 13)
-            L.control.zoom({ position: 'topright' }).addTo(this.map)
+                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; OpenStreetMap contributors'
+                }).addTo(this.map)
 
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; OpenStreetMap contributors'
-            }).addTo(this.map)
-
-            if (latInput && lngInput && latInput.value && lngInput.value) {
-                this.marker = L.marker([defaultLat, defaultLng], { draggable: true }).addTo(this.map)
-            }
-
-            this.map.on('click', (e) => {
-                const { lat, lng } = e.latlng
-                if (latInput) latInput.value = lat
-                if (lngInput) lngInput.value = lng
-                latInput?.dispatchEvent(new Event('input', { bubbles: true }))
-                lngInput?.dispatchEvent(new Event('input', { bubbles: true }))
-
-                if (this.marker) {
-                    this.marker.setLatLng([lat, lng])
-                } else {
-                    this.marker = L.marker([lat, lng], { draggable: true }).addTo(this.map)
+                if (this.lat && this.lng) {
+                    this.marker = L.marker([this.lat, this.lng], { draggable: true }).addTo(this.map)
                 }
 
-                this.marker.on('dragend', () => {
-                    const pos = this.marker.getLatLng()
-                    if (latInput) latInput.value = pos.lat
-                    if (lngInput) lngInput.value = pos.lng
-                    latInput?.dispatchEvent(new Event('input', { bubbles: true }))
-                    lngInput?.dispatchEvent(new Event('input', { bubbles: true }))
-                })
-            })
+                this.map.on('click', (e) => {
+                    const { lat, lng } = e.latlng
+                    this.lat = lat
+                    this.lng = lng
+                    this.state = lat + ', ' + lng
 
-            setTimeout(() => this.map.invalidateSize(), 200)
-        }
-    }
-}
-</script>
+                    if (this.marker) {
+                        this.marker.setLatLng([lat, lng])
+                    } else {
+                        this.marker = L.marker([lat, lng], { draggable: true }).addTo(this.map)
+                    }
+
+                    this.marker.on('dragend', () => {
+                        const pos = this.marker.getLatLng()
+                        this.lat = pos.lat
+                        this.lng = pos.lng
+                        this.state = pos.lat + ', ' + pos.lng
+                    })
+                })
+
+                setTimeout(() => this.map.invalidateSize(), 200)
+            }
+        }"
+        x-init="init()"
+        wire:ignore
+        class="leaflet-map-container rounded-lg overflow-hidden border border-gray-200 dark:border-gray-700"
+    >
+        <div x-ref="map" class="h-96 w-full"></div>
+    </div>
+</x-dynamic-component>
